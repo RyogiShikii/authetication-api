b7c25241bda508204255cf068fe2243bc836edc0
Daniel <guosongraiden@gmail.com>
2022-10-03 12:05:15 -0700

    fix bcrypt.compare error catching

--- src/controller/users.js
+++ src/controller/users.js
@@ -33,26 +33,29 @@ export const registerController = async (req,res) => {
 export const loginController = async (req,res) => {
     try {
         const { email, password } = req.body;
-    
+
         //check if email exists
         const existedUser = await userRepository
           .search()
           .where('email')
           .equals(email)
           .return.all();
-
+        if (!existedUser[0]) {
+          return res.status(401).send({ info: 'Invalid email or password' });
+        }
         //check if passowrd is correct
         const validPassword = await bcrypt.compare(
           password,
           existedUser[0].password
         );
 
-        if (!validPassword || !existedUser[0]) {
+        if (!validPassword) {
           return res.status(401).send({ info: 'Invalid email or password' });
         }
 
+
         console.log(existedUser)
-    
+
         res
           .status(200)
           .send({ info: 'login succeeded', user: existedUser[0].username });
